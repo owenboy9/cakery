7e9c900eb5b156eb0c991e52d58dfa7b589da7d9
owenboy9 <owenkraus9@gmail.com>
2025-04-16 07:55:28 +0200

    move styling to css in prod card

--- vite-project/src/components/ProductCard.jsx
+++ vite-project/src/components/ProductCard.jsx
@@ -1,43 +1,19 @@
-import '../styles/productCard.css'
+import '../styles/productCard.css';
+
 function ProductCard({ name, image, price, onBuyClick }) {
   return (
-    <div style={{
-      width: '220px',
-      backgroundColor: ' rgba(245, 227, 237, 0.37)',
-      borderRadius: '0.5rem',
-      overflow: 'hidden',
-      boxShadow: '0px 0px 10px rgba(236, 164, 97, 0.87)',
-      padding: '1rem',
-      textAlign: 'center',
-      margin: '1rem',
-      fontFamily: 'sans-serif',
-      transition: 'background-color 0.3s ease',
-    }}>
+    <div className="product-card">
       <img
         src={image}
         alt={name}
         className="product-image"
-        style={{ width: '100%', objectFit: 'cover', cursor: 'pointer', borderRadius: '0.5rem' }}
       />
-      <div style={{ padding: '0.3rem' }}>
-        <h3 style={{ fontSize: '1.5rem', margin: '0.5rem 0', color: 'black' }}>
-          {name}
-        </h3>
-        <p style={{ fontWeight: 'bold', color: 'black', margin: '0.6rem 0' }}>{price}</p>
+      <div className="product-content">
+        <h3 className="product-name">{name}</h3>
+        <p className="product-price">{price}</p>
         <button 
           className="buy-button"
           onClick={onBuyClick}
-          style={{
-            backgroundColor: 'lightpink',
-            border: 'none',
-            padding: '0.5rem 1rem',
-            borderRadius: '0.5rem',
-            cursor: 'pointer',
-            fontWeight: 'bold',
-            marginTop: '0.5rem',
-            borderShadow: '0px 0px 5px rgba(240, 121, 11, 0.5)',
-            transition: 'background-color 0.3s ease',
-          }}
         >
           buy one
         </button>
