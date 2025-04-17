c2495d73b683fd7f630b1311cf4f2fc5c7c897d7
owenboy9 <owenkraus9@gmail.com>
2025-04-17 10:41:56 +0200

    fix logo size

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,14 +1,14 @@
 // src/pages/Home.jsx
 import text from '../data/text';
 import '../styles/everyPage.css';
-
-<p>{text.home.intro}</p>
+import logo from '../assets/logo.jpg'
 
 function Home() {
   return (
     <div className="every-page-container">
       <h1>{text.home.headline}</h1>
       <h2>{text.home.subheadline}</h2>
+      <img className='logo-home' src={logo}/>
       <p>{text.home.content}</p>
       <span>{text.home.cta}</span>
     </div>
