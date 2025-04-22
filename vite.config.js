eec0eaa17208d8100b1a164947dd1bd2a34d582b
owenboy9 <owenkraus9@gmail.com>
2025-04-22 10:40:35 +0200

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/cakery/', // 👈 important for GitHub Pages
-  plugins: [react()],
-});
