d85f80f4544ed360d14e625b5296d0c759b37511
owenboy9 <owenkraus9@gmail.com>
2025-04-22 10:21:50 +0200

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/cakery/', // 👈 important for GitHub Pages
+  base: process.env.NODE_ENV === 'production' ? '/cakery/' : '/',
   plugins: [react()],
 });
